ce9f5d63ed41e421b8ce01792a42e277b8152165
ldfreedman <lfreedman@ufl.edu>
2016-11-23 16:09:16 -0500

    Server side of Network Interface and started organizing TileCreator

---
+++ src/tigerZone/NetworkInterface.java
@@ -0,0 +1,216 @@
+package tigerZone;
+
+import java.io.*;
+import java.net.*;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+public class NetworkInterface {
+	
+	static final int WAIT = 0;
+	static final int MAKE_MOVE = 1;
+	static final int OPPONENT_MOVE = 2;
+	static final int START = 3;
+	static final int DECK = 4;
+	static final int GAME_OVER = 5;
+	static final int NEW_ROUND = 6;
+	
+	static String serverPassword = "password";
+	static String ourPassword = "password";
+	static String ourPID = "groupP";
+	static String opponentPID;
+
+	 public static void main(String[] args) throws IOException {
+         
+	        if (args.length != 2) {
+	            System.err.println(
+	                "Usage: java EchoClient <host name> <port number>");
+	            System.exit(1);
+	        }
+	 
+	        String hostName = args[0];
+	        int portNumber = Integer.parseInt(args[1]);
+	 
+	        try (
+	            Socket tzSocket = new Socket(hostName, portNumber);
+	            PrintWriter out = new PrintWriter(tzSocket.getOutputStream(), true);
+	            BufferedReader in = new BufferedReader(new InputStreamReader(tzSocket.getInputStream()));
+	        ) {
+	            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+	            String fromServer;
+	            String fromUser;
+	            
+	            int x, y, rotation, state = WAIT;
+	            String cid;
+	            String rid;
+	            String gid;
+	            String pid;
+	            String rounds;
+	            ArrayList<String> deck = new ArrayList<String>();
+	            String move = "";
+	            String tile;
+	            String tileCount;
+	            String time;
+	            String ourScore;
+	            String theirScore;
+	            
+	            //while receiving from server
+	            while ((fromServer = in.readLine()) != null) {
+	            	
+	                if (fromServer.equals("THANK YOU FOR PLAYING! GOODBYE"))
+	                    break;
+	                
+	                //interpret server message
+	                StringTokenizer tokens = new StringTokenizer(fromServer, " ");
+	                String current = "";
+	                while(tokens.hasMoreTokens()){
+	                	current += tokens.nextToken();
+	                	switch(current){
+	                	case "THIS IS SPARTA!":
+	                		fromUser = "JOIN " + serverPassword;
+	                		state = WAIT;
+	                		break;
+	                	case "HELLO!":
+	                		fromUser = "I AM " + ourPID + " " + ourPassword;
+	                		state = WAIT;
+	                		break;
+	                	case "NEW CHALLENGE":
+	                		cid = tokens.nextToken();
+	                		state = WAIT;
+	                		break;
+	                	case "NEW CHALLENGE YOU WILL PLAY":
+	                		rounds = tokens.nextToken();
+	                		state = WAIT;
+	                		break;
+	                	case "BEGIN ROUND":
+	                		rid = tokens.nextToken();
+	                		state = NEW_ROUND;
+	                		break;
+	                	case "YOUR OPPONENT IS PLAYER":
+	                		opponentPID = tokens.nextToken();
+	                		state = WAIT;
+	                		break;
+	                	case "STARTING TILE IS":
+	                		deck.add(tokens.nextToken());
+	                		tile = deck.get(0);
+	                		state = WAIT;
+	                		break;
+	                	case "STARTING TILE IS AT":
+	                		x = Integer.valueOf(tokens.nextToken());
+	                		y = Integer.valueOf(tokens.nextToken());
+	                		rotation = Integer.valueOf(tokens.nextToken());
+	                		state = START;
+	                		break;
+	                	case "THE REMAINING":
+	                		tileCount = tokens.nextToken();
+	                		state = WAIT;
+	                		break;
+	                	case "THE REMAINING TILES ARE":
+	                		while(tokens.hasMoreTokens()){
+	                			deck.add(tokens.nextToken());
+	                		}
+	                		state = DECK;
+	                		break;
+	                	case "MATCH BEGINS IN":
+	                		time = tokens.nextToken();
+	                		deck.clear();
+	                		state = WAIT;
+	                		break;
+	                	case "MAKE YOUR MOVE IN GAME":
+	                		gid = tokens.nextToken();
+	                		state = MAKE_MOVE;
+	                		break;
+	                	case "MAKE YOUR MOVE IN GAME WITHIN":
+	                		time = tokens.nextToken();
+	                		state = MAKE_MOVE;
+	                		break;
+	                	case "MAKE YOUR MOVE IN GAME WITHIN SECOND: MOVE PLACE":
+	                		tile = tokens.nextToken();
+	                		state = MAKE_MOVE;
+	                		break;
+	                	case "MAKE YOUR MOVE IN GAME WITHIN SECONDS: MOVE PLACE":
+	                		tile = tokens.nextToken();
+	                		state = MAKE_MOVE;
+	                		break;
+	                	case "GAME":
+	                		gid = tokens.nextToken();
+	                		state = WAIT;
+	                		break;
+	                	case "GAME MOVE PLAYER":
+	                		pid = tokens.nextToken();
+	                		move += tokens.nextToken();
+	                		if(move == "FORFEITED:") { 
+	                			state = GAME_OVER;
+	                			while(tokens.hasMoreTokens()){
+	                				move += tokens.nextToken();
+	                			}
+	                			break;
+	                		}
+	                		tokens.nextToken();
+	                		tile = tokens.nextToken();
+	                		tokens.nextElement();
+	                		x = Integer.valueOf(tokens.nextToken());
+	                		y = Integer.valueOf(tokens.nextToken());
+	                		rotation = Integer.valueOf(tokens.nextToken());
+	                		if(pid != ourPID) { state = OPPONENT_MOVE; }
+	                		else { state = WAIT; }
+	                		break;
+	                	case "GAME OVER PLAYER":
+	                		while(tokens.hasMoreTokens()){
+	                			if(tokens.nextToken() == ourPID)
+	                				ourScore = tokens.nextToken();
+	                			else if(tokens.nextToken() == opponentPID)
+	                				theirScore = tokens.nextToken();
+	                		}
+	                		state = GAME_OVER;
+	                		break;
+	                	case "END OF ROUND":
+	                		//end current games
+	                		state = WAIT;
+	                		break;	                	
+	                	default:
+	                		state = WAIT;
+	                		break;
+	                	}
+	                	current += " ";
+	                }
+	                
+	                /////////send info to games
+	                switch(state){
+	                case WAIT:
+	                	break;
+	                case MAKE_MOVE:
+	                	break;
+	                case OPPONENT_MOVE:
+	                	break;
+	                case START:
+	                	break;
+	                case DECK:
+	                	break;
+	                case GAME_OVER:
+	                	break;
+	                case NEW_ROUND:
+	                	break;
+	                }
+	                
+	                
+	                /////////receive info from games
+	                fromUser = stdIn.readLine();
+	                
+	                ////////send info to server
+	                if (fromUser != null) {
+	                    out.println(fromUser);
+	                }
+	            }
+	            
+	        } catch (UnknownHostException e) {
+	            System.err.println("Don't know about host " + hostName);
+	            System.exit(1);
+	        } catch (IOException e) {
+	            System.err.println("Couldn't get I/O for the connection to " +
+	                hostName);
+	            System.exit(1);
+	        }
+	    }
+
+}
--- src/tigerZone/TileCreator.java
+++ src/tigerZone/TileCreator.java
@@ -1,9 +1,119 @@
 package tigerZone;
 
+import java.util.ArrayList;
+
 public class TileCreator {
 	public Territory[] myTerritories = new Territory[246];
+	
+	//Defines the preyAnimal types for tiles
+	boolean[] noPreyAnim = {false, false, false, false };
+	boolean[] deer = { true, false, false, false };
+	boolean[] boar = { false, true, false, false };
+	boolean[] buffalo = { false, false, true, false };
+	boolean[] crocodile = { false, false, false, true };
+	
+	//Defines an empty ArrayList for non jungle tiles
+	ArrayList<Integer> noBorderLakes = new ArrayList<Integer>();
+	ArrayList<Integer> noBorderDens = new ArrayList<Integer>();
+	ArrayList<Integer> borderLake = new ArrayList<Integer>();
+	
 	public TileCreator()
 	{
+		Territory ter = new Territory(0, ' ', 0, noPreyAnim, noBorderLakes, noBorderDens);
+		myTerritories[0] = ter;
+	}
+	
+	public Tile create(String toCreate){
+		Tile created = null;
+		int[] territories = new int[13];
 		
+		switch(toCreate){
+		case "JJJJ-":
+			
+			break;
+		case "JJJJX":
+			
+			break;
+		case "JJTJX":
+			
+			break;
+		case "TTTT-":
+			
+			break;
+		case "TJTJ-":
+			
+			break;
+		case "TJJT-":
+			
+			break;
+		case "TJTT-":
+			
+			break;
+		case "LLLL-":
+			
+			break;
+		case "JLLL-":
+			
+			break;
+		case "LLJJ-":
+			
+			break;
+		case "JLJL-":
+			
+			break;
+		case "LJLJ-":
+			
+			break;
+		case "LJJJ-":
+			
+			break;
+		case "JLLJ-":
+			
+			break;
+		case "TLJT-":
+			
+			break;
+		case "TLJTP":
+			
+			break;
+		case "JLTT-":
+			
+			break;
+		case "JLTTB":
+			
+			break;
+		case "TLTJ-":
+			
+			break;
+		case "TLTJD":
+			
+			break;
+		case "TLLL-":
+			
+			break;
+		case "TLTT-":
+			
+			break;
+		case "TLTTP":
+			
+			break;
+		case "TLLT-":
+			
+			break;
+		case "TLLTB":
+			
+			break;
+		case "LJTJ-":
+			
+			break;
+		case "LJTJD":
+			
+			break;
+		case "TLLLC":
+			
+			break;
+		}
+		
+		return created;
 	}
 }
